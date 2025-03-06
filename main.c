6ccb591c9edcf83dbb52388abbd1858aed35902f
Darsh Gandhi <118409878+darshgandhi@users.noreply.github.com>
2025-03-06 13:53:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <mpi.h>
+//#include <mpi.h>
 // #include "myMPI.h"   // not sure if this is needed for the teach cluster
 
 // prints E of length N to terminal
@@ -28,23 +28,23 @@ void printToFile(int* E, int N, FILE* fp) {
     fprintf(fp,"]\n");
 }
 
-// creates sequence seq of length N
-int* createSeq(int N) {
-    int* seq = (int*) malloc(N * sizeof(int));
-
-    // this loop should populate seq with {-1, +1} values
-    for (int i = 0; i < N; i++) {     
-        int randVal = rand() % 2;   // not truly random due to how rand() function is implemented
-        seq[i] = (randVal == 0) ? 1 : -1;
+// creates a sequence X for the T function
+int* createSubseq(int i, int len) {
+    int* X = (int*) malloc(len * sizeof(int));
+    for (int j=0; j<len; j++) {
+        X[j] = (i % 2) ? -1 : 1;
+        i = i / 2;
     }
-    return seq;
-}   
+    return X;
+}
 
-// unclear what this function does, creates a new sequence so the function name is inaccurate
-// I believe on further review that this function 
+// This function is called from a for loop to create sequences
+// all possible sequences of length N as i iterates from 0 to 2^N-1
 int* createSeqIdx(int N, int i) {
     int* seq = (int*) malloc(N * sizeof(int));
     for (int j = 0; j < N; j++) {
+        // pow(2, N - j - 1) calculates 2^(N-j-1), which gives the bit position value
+        // which is then used to assign to 1 or -1
         seq[j] = (i / ((int) pow(2, N - j - 1)) % 2 == 1) ? 1 : -1;
     }
     return seq;
@@ -72,7 +72,7 @@ int compute_T(int E[], int N, int k, int X[]) {
 int* createSubseq(int i, int len) {
     int* X = (int*) malloc(len * sizeof(int));
     for (int j=0; j<len; j++) {
-        X[j] = (i % 2) ? -1 : 1;
+        X[j] = (i % 2) ? 1 : -1;
         i = i / 2;
     }
     return X;
@@ -85,22 +85,28 @@ int* createSubseq(int i, int len) {
  */
 bool checkPseudorandom(int* E, int N) {
     int condition = (int) (log2(N));    // condition variable, named k in definition
-    
+
+    printf("Testing sequence length %d w/ log2(N) = %d\n", N, condition);
+
     for (int k = 1; k <= condition; k++) {  // should never loop more than 4 times in this assignment
         int kPow = (int) pow(2, k); // 2 to the power of k
         int M = N - k + 1;  // 
+        float threshold = 1.0/sqrt(N); 
 
         // Iterating through all subsequences of length k
         for (int i = 0; i < kPow; i++) {
             
             // Create subsequence X & run T
             int* X = createSubseq(i, k);
+            printArray(X, k);
             int T = compute_T(E, M, k, X);
+            float sub = (float) M / pow(2,k);
             free(X);
 
             printf("k: %d, T: %d, rVal: %f, T - rVal: %f sqrt: %f\n", k, T, fabs(T - (float) M/pow(2,k)), (1.0/sqrt(N)), 0.0);
 
-            if (fabs(T - (float) M / pow(2,k)) > (1.0/sqrt(N))) {
+            if (fabs(T - sub) > threshold) {
+                printf("FAILED: Subsequence doesn't meet randomness\n");
                 return false;   // always returned for some reason, fix asap
             }
         }
@@ -115,21 +121,22 @@ int main() {
     
     // variables for main function (in C vars should be initialized at the start of the function)
     char filename[20];
-    int N = 0;
+    int N = 10;
     int* E;
     bool random;
-    int seqLength = 0;
+    int seqLength = 10;
     FILE* file;
 
     // Implementation Question 1 Without MPI:
     E = createSeq(N);
+    printf("Testing sequence: ");
     printArray(E, N);
 
     random = checkPseudorandom(E, N);
     printf("Result: %s", random ? "True" : "False");
 
     // Implementation Question 2 With MPI for ALL 2^N sequences:
-    N = 15;
+    /*N = 15;
     seqLength = pow(2, N);
 
     // NEED TO ADD MPI STUFF SOMEWHERE
@@ -148,7 +155,7 @@ int main() {
         free(E);
     }
 
-    fclose(file);
+    fclose(file);*/
     
     // Implementation Question 4:
     //DONT RUN BELOW BEFORE MPI IMPLEMENTATION:
