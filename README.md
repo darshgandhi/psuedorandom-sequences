9b616e4f7ed4e4d422198e8b75043cf6f93cbedc
abtinturing <abtinma@yahoo.com>
2025-03-09 16:24:26 -0400

    Fixed syntax issues, made processing more efficient, and added code to gather results and merge under /scratch/pr_n.txt

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # psuedorandom-sequences
+
 CP431 Assignment 2: Psuedorandom-Sequences
+
+# Running locally
+
+Installing mpich (WSL if on Windows):
+
+```
+sudo apt install mpich
+```
+
+Compiling:
+
+```
+mpicc -o pseudorandom.exe main.c -lm -lmpi
+```
+
+Running:
+
+```
+mpirun -np <number-of-cores> ./pseudorandom.exe <N>
+```
+
+Example for N=10 with 8 cores:
+
+```
+mpirun -np 8 ./pseudorandom 10
+```
--- main.c
+++ main.c
@@ -4,8 +4,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <mpi.h>
-//#include "MyMPI.h"
+
+// We don't need this
+// #include "MyMPI.h" 
+
 #define BUFF_LENGTH 64
+
 
 // prints E of length N to terminal
 // Used for Testing
@@ -15,7 +19,7 @@ void printArray(int* E, int N) {
     printf("[");
     for (int i = 0; i < N; i++) {
         printf("%d", E[i]);
-        if(i < (N - 1)) {
+        if (i < (N - 1)) {
             printf(", ");
         }
     }
@@ -31,16 +35,16 @@ void printToFile(int* E, int N, FILE* fp) {
     fprintf(fp, "[");
     for (int i = 0; i < N; i++) {
         fprintf(fp, "%d", E[i]);
-        if(i < (N - 1)) fprintf(fp, ", ");
+        if (i < (N - 1)) fprintf(fp, ", ");
     }
-    fprintf(fp,"]: ");
+    fprintf(fp, "]: ");
 }
 
 // creates a sequence X for the T function
 int* createSubseq(int i, int k) {
     // i is integer value to convert
     // k is length of output sequence
-    int* X = (int*) malloc(k * sizeof(int));
+    int* X = (int*)malloc(k * sizeof(int));
     for (int j = 0; j < k; j++) {
         // below line is used to convert i into a bool array
         X[j] = (i & (1 << (k - j - 1))) ? 1 : -1;
@@ -53,7 +57,7 @@ int* createSubseq(int i, int k) {
 int* createSeqIdx(int N, int i) {
     // i is integer to convert
     // N is length of sequence
-    int* seq = (int*) malloc(N * sizeof(int));
+    int* seq = (int*)malloc(N * sizeof(int));
     for (int j = 0; j < N; j++) {
         // below line is used to convert i into a bool array
         seq[j] = (i & (1 << (N - j - 1))) ? 1 : -1;
@@ -71,9 +75,9 @@ int compute_T(int E[], int M, int k, int X[]) {
     for (int i = 0; i < M; i++) {
         bool identical = true;
         for (int j = 0; j < k; j++) {
-            if (E[i+j] != X[j]) { 
-                identical = false; 
-                break; 
+            if (E[i + j] != X[j]) {
+                identical = false;
+                break;
             }
         }
         if (identical) output++;
@@ -81,17 +85,17 @@ int compute_T(int E[], int M, int k, int X[]) {
     return output;
 }
 
-/* 
+/*
  * checks if subsequence is Pseudorandom or not using Knuths textbook version, by basically executing the second
- * mathematical definition in our assignment sheet 
+ * mathematical definition in our assignment sheet
  */
 bool checkPseudorandomTextbook(int* E, int N) {
     // E is the bool sequence to check 
 
     //int N = sizeof(*E) / sizeof(E[0]);   // number of elements in E
     //printf("%d\n", N);
-    int kMax = (int) floor(log2(N));    // condition variable, named k in definition
-    
+    int kMax = (int)floor(log2(N));    // condition variable, named k in definition
+
     //printf("Testing sequence length %d w/ log2(N) = %d\n", N, kMax);
 
     // Iterating through all subsequences of length k
@@ -100,18 +104,18 @@ bool checkPseudorandomTextbook(int* E, int N) {
 
         // printf("Testing K = %d\n", k);
         for (int i = 0; i < kPow; i++) {
-            
+
             // Create subsequence X & run T
             int* X = createSubseq(i, k);
             int valueT = compute_T(E, (N + 1), k, X);
             free(X);
-            
-            double sqrtN = 1.0/sqrt(N); 
+
+            double sqrtN = 1.0 / sqrt(N);
             double subVal = 1.0 / kPow;
 
-            double dValueT = (double) valueT;
+            double dValueT = (double)valueT;
 
-            double compValue = dValueT/N;
+            double compValue = dValueT / N;
             compValue -= subVal;
             compValue = fabs(compValue);
 
@@ -121,9 +125,9 @@ bool checkPseudorandomTextbook(int* E, int N) {
     return true;
 }
 
-/* 
+/*
  * checks if subsequence is Pseudorandom or not, by basically executing the second
- * mathematical definition in our assignment sheet 
+ * mathematical definition in our assignment sheet
  */
 bool checkPseudorandom(int* E, int N) {
     int kMax = (int)floor(log(N) / log(2));    // condition variable, named k in definition
@@ -134,15 +138,15 @@ bool checkPseudorandom(int* E, int N) {
 
         // printf("Testing K = %d\n", k);
         for (int i = 0; i < kPow; i++) {
-            
+
             // Create subsequence X & run T
             int* X = createSubseq(i, k);
-            int M = N + 1 - k; 
+            int M = N + 1 - k;
             int T = compute_T(E, M, k, X);
-            
-            double sqrtN = 1.0/sqrt(N); 
+
+            double sqrtN = 1.0 / sqrt(N);
             double subVal = (double)(N + 1 - k) / (1 << k); // 1 << k is the same as pow(2,k) form before
-            
+
             free(X);
 
             if (fabs(T - subVal) > sqrtN) return false;
@@ -151,7 +155,7 @@ bool checkPseudorandom(int* E, int N) {
     return true;
 }
 
-int main(int argc, char** argv) {   
+int main(int argc, char** argv) {
     // COMMENT OUT SECTIONS DEPENDING ON WHAT UR TESTING
     //=======================================================================
     // QUESTION 1
@@ -205,28 +209,29 @@ int main(int argc, char** argv) {
     FILE* localFile = fopen(localFilename, "w");
 
     // Handle Errors for file stuff
-    if (file == NULL) {
+    if (localFile == NULL) {
         perror("Error opening file");
         exit(true);
     }
 
-    // Determine workload for each process
+    // determining workload for each process
     int localSeqCount = 0;
     long long workload = totalSeq / size;
     long long startLocal = rank * workload;
     long long endLocal = 0;
     if (rank == size - 1) {
         endLocal = totalSeq;
-    } else {
-        endLocal = (rank+1)*workload
+    }
+    else {
+        endLocal = (rank + 1) * workload;
     }
 
-    startTime = MPI_Wtime();
+    start_time = MPI_Wtime();
 
-    // Each sequence can be tested independently, so divide the 2^N sequences among processes.
-    for (long long i = 0; i < totalSeq; i++) {
+    // Each process is only iterating over its assigned range of sequences
+    for (long long i = startLocal; i < endLocal; i++) {
         int* E = createSeqIdx(N, i);
-        bool random = checkPseudorandom(E, N);
+        bool random = checkPseudorandomTextbook(E, N);
 
         if (random) localSeqCount++;
         printToFile(E, N, localFile);
@@ -235,11 +240,44 @@ int main(int argc, char** argv) {
         free(E);
     }
 
-    endTime = MPI_Wtime();
+    end_time = MPI_Wtime();
 
     fclose(localFile);
 
-    // NEED TO GATHER STUFF AND THEN ALSO MERGE ALL TEMP FILES INTO A FINAL FILE BELOW
+    // gathering results and merging 
+    int globalSeqCount = 0;
+    MPI_Reduce(&localSeqCount, &globalSeqCount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        char finalFilename[BUFF_LENGTH];
+        snprintf(finalFilename, sizeof(finalFilename), "scratch/pr_%d.txt", N);
+
+        FILE *finalFile = fopen(finalFilename, "w");
+        if (finalFile == NULL) {
+            perror("Error opening final output file");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        }
+        
+        char localFilename[BUFF_LENGTH];
+        char line[1024];
+        for (int r = 0; r < size; r++) {
+            snprintf(localFilename, sizeof(localFilename), "local_pr_%d.%d.txt", r, N);
+            FILE *localFile = fopen(localFilename, "r");
+            while (fgets(line, sizeof(line), localFile) != NULL) {
+                fputs(line, finalFile);
+            }
+            fclose(localFile);
+            remove(localFilename);
+
+        }
+        
+        fprintf(finalFile, "\nTotal pseudorandom sequences: %d\n", globalSeqCount);
+        fclose(finalFile);
+        printf("Merged final output written to %s\n", finalFilename);
+    }
+
     // Keep MPI  FINALIZe below everything
     MPI_Finalize();
 }
